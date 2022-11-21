47ba30470393ec27b47a88317819cd0f19811dc8
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-11-21 14:47:56 -0500

    Simple first code in C

---
+++ C/Kernighan_Ritchie/first_code.c
@@ -0,0 +1,10 @@
+//Author: Rohan Singh
+//Simple Hello World Code in C
+
+#include<stdio.h>
+
+int main(){
+  //Using the printf function to print "Hello World"
+  printf("Hello World!\n");
+  return 0;
+}
