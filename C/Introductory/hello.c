0af6fe4b5ec26345bd6c678a01c521e9d597c4a8
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-12 23:06:30 -0500

    Added Hello

---
+++ C/Introductory/hello.c
@@ -0,0 +1,35 @@
+//  Author: Rohan Singh
+//  1/12/2023
+//  Hello World code in C
+
+
+//  Imports
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//  Main method
+int main(){
+
+    //Print Method
+    printf("Hello World!\n");
+
+    //Printing an interger
+    printf("My age is: %d\n",18);
+
+    //Printing an integer in hexadecimal
+    printf("My age in Hex is: %x\n",18);
+
+    //Printing an integer in octal
+    printf("My age is: %o\n",18);
+    
+    //Printing the decimal of the hexadecimal
+    printf("My age is: %d\n",0xFFFF);
+
+    //Printing the decimal of the octal
+    printf("My age is: %d\n",07777);
+
+    //Printing a String
+
+    return 0;
+}
