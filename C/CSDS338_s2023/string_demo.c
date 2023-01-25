46fee0ee4803740ecb3894062603aa1662dbf2d3
Rohan-s18 <Rohan-s18@github.com>
2023-01-24 21:31:44 -0500

    Added String DEmo

---
+++ C/CSDS338_s2023/string_demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char my_name[8];
+    printf("The address location of my_name is: %p\n",my_name);
+    strcpy(my_name,"Rohan");
+    printf("The value of my_name : %s\n",my_name);
+    return 0;
+}
