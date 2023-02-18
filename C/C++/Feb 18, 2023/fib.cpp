c071701a518706be7bab0580865721c27edf8b37
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-18 17:01:55 -0500

    Added Code for Dynamic Programming demonstration

--- C/C++/Feb 18, 2023/fib.cpp
+++ C/C++/Feb 18, 2023/fib.cpp
@@ -3,14 +3,40 @@
 //  C++ code to explain how dynamic programming works
 
 
-using namespace std;
-
 #include <iostream>
 
 class Fibonacci{
 
+    public:
+
+    int recur(int n){
+        if(n==1 || n==2)
+            return 1;
+        return recur(n-1)  +recur(n-2);
+    }
+
+    int iter(int n){
+        int f[n];
+        f[0] = 1;
+        f[1] = 1;
+        for(int i = 2; i < n; i++){
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[n-1];
+    }
+
 };
 
 int main(){
+
+    Fibonacci* fib = new Fibonacci();
+
+    std::cout<<fib->iter(45);
+    std::cout<<"\n";
+
+    std::cout<<fib->recur(45);
+    std::cout<<"\n";
+
+
     return 0;
 }
