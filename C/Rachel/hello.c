3066c141f2aeee5a8ddfa7725f1fd1fb5049579c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-09 18:22:48 -0500

    Rachel

---
+++ C/Rachel/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello World!\n");
+
+    printf("My name is: %s\n","Rachel");
+
+    printf("I am %d years old\n",18);
+
+    printf("My sages grade is :%f\n",0.88);
+
+    printf("My age in hexadecimal is :%x\n",18);
+
+    return 0;
+}
