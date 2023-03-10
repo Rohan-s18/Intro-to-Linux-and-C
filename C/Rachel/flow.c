4341f89ae0e8fc17aad158c3a5a1ed4f4f5c377e
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-09 20:02:42 -0500

    Flow

---
+++ C/Rachel/flow.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    for(int i = 0; i < 100; i++){
+        printf("System Verilog!\n");
+
+    }
+
+    int n = 0; 
+    while(n < 100){
+        printf("For each ehhn in the domehhn\n");
+        n++;
+    }
+
+    int a = 100;
+    do{
+        printf("ah!\n");
+        a--;
+    }
+    while(a >= 0);
+
+    return 0;
+}
