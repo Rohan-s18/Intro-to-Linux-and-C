a220899561df295640e91f1f2e0abcfe416c9f9c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-12 00:53:41 -0500

    Added basic code for structs

---
+++ C/Rachel/structs.c
@@ -0,0 +1,34 @@
+//  Author: Rohan Singh
+//  Code for Structs in C that I wrote for Rachie Tjarky
+//  3/12/2023
+
+
+//  Imports
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//  Struct for Movie
+struct Movie{
+
+};
+
+
+//  Printing function
+void printStruct(struct Movie movie){
+
+}
+
+
+//  Accesser function
+void changeRating(struct Movie* movie, int rating){
+
+}
+
+
+
+//  Main function
+int main(){
+
+    return 0;
+}
